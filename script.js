f375004feb70c5fd4306e97c93750006e11f46ae
Dipesh Patel <dipeshgpatel@gmail.com>
2021-12-25 09:45:18 -0800

    Game logic added and working, needs refining. Created X and O symbols in CSS. Reset button with animation created with CSS. Added img folder and reset button image.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let playerX = [];
 let playerO = [];
-const player_X = 'X';
-const player_O = 'O';
-let currentTurn = 0;
+//const player_X = 'X';
+//const player_O = 'O';
+let turnCounter = 1;
+//let playerOTurn = false;
+//let winCounter = 0;
 const winningConditions = [
     // Rows
     ['0', '1', '2'],
@@ -19,8 +21,8 @@ const winningConditions = [
     ['2', '4', '6']
 
 ];
-let playerXPoints = 0;
-let playerOPoints = 0;
+// let playerXPoints = 0;
+// let playerOPoints = 0;
 
 const gameBoard = document.querySelector('.gameBoard');
 console.log(gameBoard);
@@ -36,4 +38,89 @@ console.log(playerXIcon);
 
 const playerOIcon = document.querySelector('#playerOicon');
 console.log(playerOIcon);
+let turnText = document.querySelector(".turnMessage")
+
+window.onload = startGame;
+
+
+function startGame() {
+    gameListener();
+    resetGame();
+}
+
+function gameListener() {
+    for (let i = squares.length -1; i >= 0; i--) {
+        squares[i].addEventListener('click', playerSymbols);
+    }
+}
+
+function playerSymbols(event){
+    console.log(event.target.innerHTML.length)
+    if (event.target.innerHTML.length === 0){
+      if (turnCounter % 2 === 0) {
+        playerO.push(event.target.getAttribute("id"));
+        event.target.innerHTML = " ";
+        event.target.setAttribute("XO","O");
+        turnText.innerHTML = "It is X's turn";
+        turnCounter++;
+        if(check(playerO)){        
+            alert("Congrats player two you win");
+            resetBoard();
+            return true;
+        }
+      }
+      else {
+        playerX.push(event.target.getAttribute("id"));
+        event.target.innerHTML = " ";
+        event.target.setAttribute("XO","X");
+        turnText.innerHTML = "It is O's turn";
+        turnCounter++;
+        if(check(playerX)){
+            alert("Congrats player one you win");
+            resetBoard();
+            return true;
+          }
+      }
+    // if the counter is greater than or equal to 10, the game is a draw!
+    if (turnCounter >= 10){
+      turnText.innerHTML = "Game Over!";
+      let confirmed = confirm("It's a draw, do you want to play again?");
+      if(confirmed){
+        resetBoard();
+      }
+    }
+   }
+  }
+  
+  function resetGame(){
+    reset.addEventListener("click", resetBoard);
+  }
+
+  function check(array){
+    let finalResult = false;
+    for(let item of winningConditions){
+    
+      let result = item.every(val => array.indexOf(val) !== -1);
+      // console.log(array)
+      // console.log(item)
+      // console.log(res)
+      if(result){
+        finalResult = true;
+        break;
+      }
+    }
+    return finalResult;
+  }
+
+  function resetBoard(){
+    for (let i = squares.length - 1; i >= 0; i--) {
+      squares[i].innerHTML="";
+      squares[i].setAttribute("XO","");
+    }
+    playerO = [];
+    playerX = [];
+    //winCounter=0;
+    turnCounter = 1;
+    turnText.innerHTML = "It is X's turn";
+  }
 
